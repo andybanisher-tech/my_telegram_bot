19ea3918a97412240b5055e88115710d225c794c
Andrey Scherbina <outs1der@bk.ru>
2026-04-08 20:38:03 +0300

    рефакторинг: удалены неиспользуемые импорты и код из обработчиков администраторов и менеджеров

--- handlers/admin/__init__.py
+++ handlers/admin/__init__.py
@@ -2,5 +2,5 @@ from . import news
 from . import categories
 from . import admins
 from . import stats
-from . import managers  # добавлено
-from . import set_bitrix_key  # добавлено
+from . import managers
+from . import set_bitrix_key
